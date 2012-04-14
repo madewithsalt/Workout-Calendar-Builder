28d8ff446d088099149aa99ef9f054c7b5790449
bonnerk <kristi@theresnobox.net>
2012-04-14 15:03:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# How to use
+
+This is a silly script I wrote to create a CSV file that's compatible with Google Calendar.
+
+** Requirements **
+- A terminal window
+- Ruby
+
+
+** Example Usage **
+	$ ruby p90x-csv.rb "April 16, 2012", "7:30 AM", "classic"
+
+It's a pretty stupid program. You have to use this order, and "classic" is the only thing supported right now.
+
+#Adding the Calendar to Google
+
+- MAKE A NEW CALENDAR. Seriously. You don't want to delete 90 individual entries if you messed up your CSV.
+- Go to your calendar settings and select "Import Calendar"
+- MAKE SURE YOU SELECT YOUR NEW CALENDAR. I warned you!
+
+
+** Pull requests welcome! **
+Eventually I'll make this into a cute little web-page, but until then its for the enjoyment of us hardcore nerds only. :)
+
+I was too lazy to add doubles and leans, but it's ready to be added to the JSON.
---
+++ p90x-csv.rb
@@ -0,0 +1,54 @@
+require 'json'
+require 'date'
+require 'time'
+require 'active_support'
+
+class Parser
+	def initialize(start_date, time, type)
+    @file = File.read('p90x-data.json')
+		@data = JSON.parse(@file)
+    @file_name = "p90x_#{type}.csv"
+
+    parse_file_data(start_date, time, type)
+  end
+  def parse_file_data(date, time, type)
+    get_date = Date.parse(date)
+    get_time = DateTime.parse("#{get_date} #{time}")
+    workouts = @data['workouts']
+    events = @data[type]
+
+    #start with the top of the csv = google calendar style
+    parsed_data = "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Reminder On/Off,\
+                  Reminder Date,Reminder Time,Meeting Organizer,Description,Location,Private \r\n"
+
+    # now for each of the 90 days we'll create calendar events based on the start date
+    (1..90).each { |n|
+      agenda = ''
+      current_week = n/7.ceil + 1
+      start_date = (get_date + n).strftime('%m/%d/%y')
+      subject = workouts[events[n-1]]
+      start_time = get_time.strftime('%T')   #2/14/2008,8:10:00
+      end_time = ''
+      end_date = start_date
+      all_day = "FALSE"
+      description = ''
+      location = "Your Home Gym!"
+      private = "TRUE"
+
+      agenda += "\"#{subject}\",#{start_date},#{start_time},#{end_date},#{end_time},#{all_day},#{description},#{location},#{private} \r\n"
+      parsed_data += agenda
+    }
+    puts parsed_data
+    save_file_data(parsed_data)
+  end
+  def save_file_data(parsed_data)
+    puts "Saving file data to #{@file_name}..."
+
+    @new_file = File.open("#{@file_name}", "w")
+    @new_file.write(parsed_data)
+  end
+end
+
+# EXAMPLE USE: Takes 3 values, Start Date, Start Time, and mode. Only Classic is in now.
+#parser = Parser.new("April 18, 2012", "7:30 AM", "classic")
+parser = Parser.new(ARGV[0], ARGV[1], ARGV[2])
